7805aa9c652a28eaff9a474d5222a1e484637971
ivanoff-ivan <aenlordish@gmail.com>
2022-04-25 22:44:20 +0300

    Reset to Workshop III commit + Added environmental variables in settings.py

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECURITY_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = True
 
