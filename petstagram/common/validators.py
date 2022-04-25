d6a0fa58001c503185a6dab34b7644ab3dba4989
ivanoff-ivan <aenlordish@gmail.com>
2022-04-25 22:53:20 +0300

    Common code cleanup

--- petstagram/common/validators.py
+++ petstagram/common/validators.py
@@ -1,22 +1,11 @@
 from django.core.exceptions import ValidationError
-
-# def always_valid(chars):
-#     def validator1123(value):
-#         pass
-#
-#     return validator1123
 from django.utils.deconstruct import deconstructible
 
 
 def validate_only_letters(value):
     for ch in value:
         if not ch.isalpha():
-            # Invalid case
             raise ValidationError('Value must contain only letters')
-    # valid case
-    #
-    # if not all(ch.isalpha() for ch in value):
-    #     raise ValidationError('Value must contain only letters')
 
 
 def validate_file_max_size_in_mb(max_size):
